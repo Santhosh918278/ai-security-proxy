9e252817f5360b8f3fc490947043cae5366bed59
Santhosh kumar <vankudothsanthosh92@gmail.com>
2026-09-14 12:29:58 +0530

    Switch to cloud databases (Neon Postgres + MongoDB Atlas)

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -1,7 +1,12 @@
 const { Pool } = require("pg");
 const config = require("../config");
 
-const pool = new Pool(config.pg);
+const pool = config.pgConnectionString
+  ? new Pool({
+      connectionString: config.pgConnectionString,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool(config.pg);
 
 pool.on("error", (err) => {
   console.error("Unexpected Postgres error on idle client", err);
